19b5968d83c256974dc7e73369c2aa2fcf4c1685
VisualityD <vrednyyvv@gmail.com>
2025-06-14 18:25:57 +0300

    Delete mschat directory

--- mschat/other/generator.php
+++
@@ -1,8 +0,0 @@
-<?
-header("Content-type: text/html;charset=utf-8");
-//генератор для создания текста для смайлов dat
-for($i=1;$i<93;$i++){
-	$s = $i + 75;
-	echo "3^*$s*^Смайл^skype/$i.gif^<br>";
-	}
-?>
